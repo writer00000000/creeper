64d91252a57bc6de8ac496a34256ca01f377ea7d
writer00000000 <gnfuak@naver.com>
2022-10-04 18:09:45 +0900

    저장용

---
+++ src/main/java/net/writer1062/creepergirlmod/CreeperGirlMod.java
@@ -0,0 +1,15 @@
+package net.writer1062.creepergirlmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreeperGirlMod implements ModInitializer {
+	public static final String MOD_ID = "creepergirlmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/writer1062/creepergirlmod/CreeperGirlModClient.java
@@ -0,0 +1,10 @@
+package net.writer1062.creepergirlmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreeperGirlModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
